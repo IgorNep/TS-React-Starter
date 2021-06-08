191498e1492ac4713908a0d699398de64ca5972e
igornep <igornesan@gmail.comm>
2021-06-08 17:38:47 +0300

    finish with styles and images

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import './styles.css';
+import image from '../assets/react-logo.png';
+import logo from '../assets/react-1.svg';
+
 export const App = () => {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <h1>Hello world</h1>;
+      <img src={image} alt='React Logo' width='300' height='200' />
+      <img src={logo} alt='React Logo Svg' width='300' height='200' />
+    </>
+  );
 };
